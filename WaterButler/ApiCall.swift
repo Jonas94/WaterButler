9f68a4517946a43b2ff605ec54bdf13c577718b8
Jonas Fredriksson <jonas.fredriksson@comeon.com>
2021-06-03 20:36:00 +0200

    formatting of apicall

--- WaterButler/ApiCall.swift
+++ WaterButler/ApiCall.swift
@@ -8,8 +8,8 @@
 import Foundation
 
 class ApiCall {
-        
-        func saveScheduledItem(scheduledItem : ScheduledItem) {
+    
+    func saveScheduledItem(scheduledItem : ScheduledItem) {
         
         // Prepare URL
         let url = URL(string: UserDefaults.standard.string(forKey: "piwater_url")!+"/saveRecurringWatering")
@@ -17,28 +17,28 @@ class ApiCall {
         // Prepare URL Request Object
         var request = URLRequest(url: requestUrl)
         request.httpMethod = "POST"
-         
+        
         let postString = "id=\(scheduledItem.id)&days=\(scheduledItem.days.joined(separator: ","))&time=\(scheduledItem.time)&duration=\(Int(scheduledItem.duration))&active=\(scheduledItem.active)";
         // Set HTTP Request Body
         request.httpBody = postString.data(using: String.Encoding.utf8);
         // Perform HTTP Request
         let task = URLSession.shared.dataTask(with: request) { (data, response, error) in
-                
-                // Check for Error
-                if let error = error {
-                    print("Error took place \(error)")
-                    return
-                }
-         
-                // Convert HTTP Response Data to a String
-                if let data = data, let dataString = String(data: data, encoding: .utf8) {
-                    print("Response data string:\n \(dataString)")
-                }
+            
+            // Check for Error
+            if let error = error {
+                print("Error took place \(error)")
+                return
+            }
+            
+            // Convert HTTP Response Data to a String
+            if let data = data, let dataString = String(data: data, encoding: .utf8) {
+                print("Response data string:\n \(dataString)")
+            }
         }
         task.resume()
-       }
+    }
     
-    func loadCurrentWatering(scheduledItem : ScheduledItem) {
+    func loadCurrentWatering() {
         
         // Prepare URL
         let url = URL(string: UserDefaults.standard.string(forKey: "piwater_url")!+"/loadCurrentWatering")
@@ -46,28 +46,28 @@ class ApiCall {
         // Prepare URL Request Object
         var request = URLRequest(url: requestUrl)
         request.httpMethod = "POST"
-         
+        
         let postString = "id=\(scheduledItem.id)&days=\(scheduledItem.days.joined(separator: ","))&time=\(scheduledItem.time)&duration=\(Int(scheduledItem.duration))&active=\(scheduledItem.active)";
         // Set HTTP Request Body
         request.httpBody = postString.data(using: String.Encoding.utf8);
         // Perform HTTP Request
         let task = URLSession.shared.dataTask(with: request) { (data, response, error) in
-                
-                // Check for Error
-                if let error = error {
-                    print("Error took place \(error)")
-                    return
-                }
-         
-                // Convert HTTP Response Data to a String
-                if let data = data, let dataString = String(data: data, encoding: .utf8) {
-                    print("Response data string:\n \(dataString)")
-                }
+            
+            // Check for Error
+            if let error = error {
+                print("Error took place \(error)")
+                return
+            }
+            
+            // Convert HTTP Response Data to a String
+            if let data = data, let dataString = String(data: data, encoding: .utf8) {
+                print("Response data string:\n \(dataString)")
+            }
         }
         task.resume()
         
         
-       }
+    }
     
     
     
