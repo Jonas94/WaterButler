4bfc66053efeea285320ec2a0c16407e7cf4bed7
Jonas Fredriksson <jonas.fredriksson@comeon.com>
2021-06-03 14:35:13 +0200

    Alot of changes in the edit view, including formatting

--- WaterButler/EditScheduledItemView.swift
+++ WaterButler/EditScheduledItemView.swift
@@ -9,96 +9,116 @@ import SwiftUI
 
 struct EditScheduledItemView: View {
     @ObservedObject var scheduledItem : ScheduledItem
+    
     @State var date = Date()
     @State var editMode :Bool
+    @State var sundayEnabled : Bool = false
+    let apiCall = ApiCall()
+    let days : [String] = ["mon" , "tue" , "wed" , "thu" , "fri" , "sat" , "sun"]
+    let translatedDays : [String] = ["Måndag" , "Tisdag" , "Onsdag" , "Torsdag" , "Fredag" , "Lördag" , "Söndag"]
     
-    
-    
-
     var body: some View {
-
+        
+        VStack() {
+            DatePicker("",
+                       selection: getTime(scheduledItem: scheduledItem),
+                       displayedComponents: .hourAndMinute
+            )
+            .datePickerStyle(WheelDatePickerStyle())
+            .labelsHidden()
+            .onChange(of: date, perform: { value in
+                date = value
+            })
+            .padding()
             
-            VStack() {
-                DatePicker("",
-                           selection: getTime(scheduledItem: scheduledItem),
-                           displayedComponents: .hourAndMinute
-                           )
-                    .datePickerStyle(WheelDatePickerStyle())
-                    .labelsHidden()
-                    .onChange(of: date, perform: { value in
-                        date = value
-                    })
-                    .padding()
-            
-                HStack{
-                VStack{
-                    
-                    Toggle("Måndag", isOn: isEnabled(day: "mon", scheduledItem: scheduledItem))
-                    Toggle("Tisdag", isOn: isEnabled(day: "tue", scheduledItem: scheduledItem))
-                    Toggle("Onsdag", isOn: isEnabled(day: "wed", scheduledItem: scheduledItem))
-                    Toggle("Torsdag", isOn :isEnabled(day: "thu", scheduledItem: scheduledItem))
-
-
-                }.padding()
-                    
-                    VStack{
-
-                    Toggle("Fredag", isOn: isEnabled(day: "fri", scheduledItem: scheduledItem))
-                    Toggle("Lördag", isOn: isEnabled(day: "sat", scheduledItem: scheduledItem))
-                    Toggle("Söndag", isOn: isEnabled(day: "sun", scheduledItem: scheduledItem))
-                            
-                    }.padding()
-                }.padding()
+            HStack{
                 
-                HStack {
-                    Image(systemName: "minus")
-                    Text("🌵")
-                    Slider(value: getDuration(scheduledItem: scheduledItem), in: 0...30, step: 1)
-//
-                        .padding()
-                        .accentColor(Color.blue)
-                        .overlay(RoundedRectangle(cornerRadius: 15.0)
-                                    .stroke(lineWidth: 3.0)
-                                    .foregroundColor(Color.green))
-                    Text("💦")
-                    Image(systemName: "plus")
-                }.foregroundColor(Color.green).padding()
-                Text("Vattna i \(scheduledItem.duration, specifier: "%.0f") minuter")
-        }
-        .navigationTitle(editMode ? "Ändra" : "Lägg till")
-        .navigationBarTitleDisplayMode(.inline)
-            .navigationBarItems(trailing: (
-                Button(action: {
-                    print("button pressed")
-                    saveData(scheduledItem: scheduledItem)
-
-                }) {
-                    Image(systemName: "checkmark")
-                        .imageScale(.large)
-                }
-            ))
+                VStack{
+      
+                    ForEach(0..<4, id: \.self){ key in
+                     
+                     Toggle(isOn: Binding<Bool>(get: { scheduledItem.days.contains(days[key]) },
+                     
+                     set: {
+                     
+                     if $0 {
+                     self.scheduledItem.days.append(days[key])
+                     } else {
+                     self.scheduledItem.days = self.scheduledItem.days.filter { $0 != days[key] }
+                     }
+                     })) {
+                     Text(translatedDays[key])
+                     }
+               }}.padding()
+            
+            VStack{
+                ForEach(4..<days.count, id: \.self){ key in
+                 
+                 Toggle(isOn: Binding<Bool>(get: { scheduledItem.days.contains(days[key]) },
+                 
+                 set: {
+                 
+                 if $0 {
+                 self.scheduledItem.days.append(days[key])
+                 } else {
+                 self.scheduledItem.days = self.scheduledItem.days.filter { $0 != days[key] }
+                 }
+                 })) {
+                 Text(translatedDays[key])
+                 }
+           }
+            }.padding()
+        }.padding()
+        
+        HStack {
+            Image(systemName: "minus")
+            Text("🌵")
+            Slider(value: getDuration(scheduledItem: scheduledItem), in: 0...30, step: 1)
+                //
+                .padding()
+                .accentColor(Color.blue)
+                .overlay(RoundedRectangle(cornerRadius: 15.0)
+                            .stroke(lineWidth: 3.0)
+                            .foregroundColor(Color.green))
+            Text("💦")
+            Image(systemName: "plus")
+        }.foregroundColor(Color.green).padding()
+        Text("Vattna i \(scheduledItem.duration, specifier: "%.0f") minuter")
     }
+    .navigationTitle(editMode ? "Ändra" : "Lägg till")
+    .navigationBarTitleDisplayMode(.inline)
+    .navigationBarItems(trailing: (
+    Button(action: {
+    print("button pressed")
+    saveData(scheduledItem: scheduledItem)
+    
+    }) {
+    Image(systemName: "checkmark")
+    .imageScale(.large)
+    }
+    ))
 }
-
+}
 
 func isEnabled(day : String, scheduledItem : ScheduledItem) -> Binding<Bool> {
     let apiCall = ApiCall()
-
+    
     return Binding<Bool>(
         get: { scheduledItem.days.contains(day) },
-            set: { newValue in
-                if(newValue) {
+        set: { newValue in
+            if(newValue) {
                 scheduledItem.days.append(day)
-                }
-                else{
-                    let indexOfDay = scheduledItem.days.firstIndex(of: day)
-                    scheduledItem.days.remove(at: indexOfDay!)
-                }
-                apiCall.saveData(scheduledItem: scheduledItem)
-                
-            })
+            }
+            else{
+                let indexOfDay = scheduledItem.days.firstIndex(of: day)
+                scheduledItem.days.remove(at: indexOfDay!)
+            }
+            apiCall.saveData(scheduledItem: scheduledItem)
             
+        })
+    
 }
+
 
 
 func getTime(scheduledItem: ScheduledItem) -> Binding<Date> {
@@ -108,21 +128,21 @@ func getTime(scheduledItem: ScheduledItem) -> Binding<Date> {
         get: {
             dateFormatter.date(from:scheduledItem.time)!
         },
-            set: {newValue in
-                scheduledItem.time = dateFormatter.string(from: newValue)
-                })
+        set: {newValue in
+            scheduledItem.time = dateFormatter.string(from: newValue)
+        })
 }
 
 func getDuration(scheduledItem: ScheduledItem) -> Binding<Double> {
-
+    
     Binding<Double>(
         get: {
             Double(scheduledItem.duration)
         },
-            set: { newValue in
-                scheduledItem.duration = newValue
-                scheduledItem.objectWillChange.send()
-                })
+        set: { newValue in
+            scheduledItem.duration = newValue
+            scheduledItem.objectWillChange.send()
+        })
 }
 
 func saveData(scheduledItem : ScheduledItem) {
@@ -133,31 +153,31 @@ func saveData(scheduledItem : ScheduledItem) {
     // Prepare URL Request Object
     var request = URLRequest(url: requestUrl)
     request.httpMethod = "POST"
-     
+    
     let postString = "id=\(scheduledItem.id)&days=\(scheduledItem.days.joined(separator: ","))&time=\(scheduledItem.time)&duration=\(Int(scheduledItem.duration))";
     // Set HTTP Request Body
     request.httpBody = postString.data(using: String.Encoding.utf8);
     // Perform HTTP Request
     let task = URLSession.shared.dataTask(with: request) { (data, response, error) in
-            
-            // Check for Error
-            if let error = error {
-                print("Error took place \(error)")
-                return
-            }
-     
-            // Convert HTTP Response Data to a String
-            if let data = data, let dataString = String(data: data, encoding: .utf8) {
-                print("Response data string:\n \(dataString)")
-            }
+        
+        // Check for Error
+        if let error = error {
+            print("Error took place \(error)")
+            return
+        }
+        
+        // Convert HTTP Response Data to a String
+        if let data = data, let dataString = String(data: data, encoding: .utf8) {
+            print("Response data string:\n \(dataString)")
+        }
     }
     task.resume()
     
     
-   }
+}
 
-    struct EditScheduledItemView_Previews: PreviewProvider {
-        static var previews: some View {
-            EditScheduledItemView(scheduledItem: ScheduledItem(id: UUID().uuidString, time: "22:00", duration: 3, days: ["Mån", "Ons", "Tors", "Fre"], active: false), editMode: true)
-        }
+struct EditScheduledItemView_Previews: PreviewProvider {
+    static var previews: some View {
+        EditScheduledItemView(scheduledItem: ScheduledItem(id: UUID().uuidString, time: "22:00", duration: 3, days: ["Mån", "Ons", "Tors", "Fre"], active: false), editMode: true)
     }
+}
