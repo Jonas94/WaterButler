75491b1c8c758dc5d1fc3b7381bb1429308d3042
Jonas Fredriksson <jonas.fredriksson@comeon.com>
2021-06-05 20:15:13 +0200

    created empty view for showing success screen on scheduling

---
+++ WaterButler/View/ScheduledSuccessView.swift
@@ -0,0 +1,20 @@
+//
+//  ScheduledSuccessView.swift
+//  WaterButler
+//
+//  Created by Jonas Fredriksson on 2021-06-05.
+//
+
+import SwiftUI
+
+struct ScheduledSuccessView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ScheduledSuccessView_Previews: PreviewProvider {
+    static var previews: some View {
+        ScheduledSuccessView()
+    }
+}
